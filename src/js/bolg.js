db2349e69c0384f2849c2b743065908107d21666
AbdulManan <abdulmana.ahmadi04@gmail.com>
2026-04-30 18:34:21 +0430

    adedd navbar and footer the bolg section

--- src/js/bolg.js
+++ src/js/bolg.js
@@ -1,15 +1,1 @@
-const btn = document.getElementById("themeToggle");
-const root = document.documentElement;
-
-btn.addEventListener("click", ()=>{
-
-   root.classList.toggle("dark");
-
-   if(root.classList.contains("dark")){
-      btn.innerHTML="Light";
-   }else{
-      btn.innerHTML="Dark";
-   }
-
-});
 
